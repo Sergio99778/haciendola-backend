690b53a5f1d4fe4533104bfdb9c24d416b834479
sergio99778 <sergio4brandon@gmail.com>
2024-05-08 17:36:17 -0600

    Initial version auth with jwt

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+// Extra modules
+
+import { PassportModule } from '@nestjs/passport';
+
 @Module({
-  imports: [],
+  imports: [PassportModule.register({ defaultStrategy: 'jwt' })],
   controllers: [AppController],
   providers: [AppService],
 })
