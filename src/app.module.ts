a335be9aa789e87d9ea70b64c23f363cdb3df267
sergio99778 <sergio4brandon@gmail.com>
2024-05-08 19:58:35 -0600

    initial config module env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,28 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-// Extra modules
-
 import { PassportModule } from '@nestjs/passport';
 import { ProductsModule } from './products/products.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     PassportModule.register({ defaultStrategy: 'jwt' }),
     ProductsModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [],
+      synchronize: true, // not use in production
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
